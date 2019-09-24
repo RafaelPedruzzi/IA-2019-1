e91d908afc5124a6d8aad2f47918ffdd4d853a88
RafaelPedruzzi <nivereth@gmail.com>
2019-09-24 10:24:01 -0300

    Updating genetic

--- genetic.py
+++ genetic.py
@@ -7,30 +7,61 @@
 ## -------------------------------------------------------- ##
 
 import bagProblem as bp
+import random
+from math import floor
+
+# Objective function:
+def fitness(s):
+    if bp.state_Verify(s):
+        return bp.state_Value(s)
+    return 0
+
+# Returns the best individual in the population:
+def best_in_Pop(si):
+    sn = 0 # best state index
+    for i in range(len(si)):
+        if bp.state_Value(si[i]) > bp.state_Value(si[sn]):
+            sn = i
+    return si[sn]
+
+# Returns the index to the worse individual in the population:
+def worse_in_Pop(si):
+    sn = 0 # worse state index
+    for i in range(len(si)):
+        if bp.state_Value(si[i]) < bp.state_Value(si[sn]):
+            sn = i
+    return sn
 
 # Returns a initial population:
-def init_Population():
+def init_Population(popMaxSize):
+    pop = []
+    for _ in range(popMaxSize):
+        s = []
+        for i in range(len(bp.OBJs)):
+            v = floor(bp.size(bp.OBJs[i]) * random.random())
+            s.append(v)
+        random.shuffle(s)
+        pop.append(s)
+    return pop
 
 # Generates a new population:
-def generate_New_Pop(si):
+def generate_New_Pop(si, elite):
 
-# Select the most fit states in the given population:
+# Select the most fit individuals in the given population:
 def select_Most_Fit(si):
 
-# Returns the best state in the population:
-def best_in_Pop(si):
-
 # Genetic Algorithm:
-def genetic(n, iter):
-    si = init_Population()
+def genetic(popMaxSize, iter):
+    si = init_Population(popMaxSize)
     bs = [0]*len(bp.OBJs)
     for _ in range(iter):
-        ss = generate_New_Pop(select_Most_Fit(si))
+        ss = generate_New_Pop(select_Most_Fit(si), best_in_Pop(si))
         s = best_in_Pop(ss)
         if bp.state_Verify(s) and bp.state_Value(s) > bp.state_Value(bs):
             bs = s
+        si = ss
     return bs
 
 iter = 50
-n = 10
-print(genetic(n,iter))
+popMaxSize = 10
+#print(genetic(popMaxSize,iter))
