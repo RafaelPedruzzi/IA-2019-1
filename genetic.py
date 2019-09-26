b822e01f534660b5b12ce9d63016d701c3d2affb
RafaelPedruzzi <nivereth@gmail.com>
2019-09-26 20:39:09 -0300

    Adding coments in genetic

--- genetic.py
+++ genetic.py
@@ -38,16 +38,16 @@ def init_Population(popMaxSize):
 
 # Crossover operation:
 def crossover(s1, s2):
-    cut = random.randint(1,len(s1)-1)
+    cut = random.randint(1,len(s1)-1) # selecting a random cut point
     child1 = s1[:cut] + s2[cut:]
     child2 = s2[:cut] + s1[cut:]
     return child1, child2
 
 # Mutation operation:
 def mutation(s):
-    for _ in range(random.randint(0, len(s)-1)):
-        target = random.randint(0, len(s)-1)
-        if random.random() >= 0.5:
+    for _ in range(random.randint(0, len(s)-1)): # randomly selecting the number of changes
+        target = random.randint(0, len(s)-1) # randomly selecting the position to be changed
+        if random.random() >= 0.5: # randomly selecting the change (increment or decrement)
             s[target] += 1
         elif s[target] > 0:
             s[target] -= 1
@@ -79,14 +79,14 @@ def generate_New_Pop(si, crossoverRate, mutationRate):
     elite = best_in_Pop(si)
     snew = [elite]
     while len(snew) < len(si):
-        s1 = roulette(si)
-        if random.random() < crossoverRate:
+        s1 = roulette(si) # selecting a individual by the roulette method
+        if random.random() < crossoverRate: # crossover operation
             s2 = roulette(si)
             s1, s2 = crossover(s1, s2)
             snew.append(s2)
-            if len(snew) >= len(si):
+            if len(snew) >= len(si): # breaking if population is full
                 break
-        if random.random() < mutationRate:
+        if random.random() < mutationRate: # mutation operation
             s1 = mutation(s1)
         snew.append(s1)
     return snew
@@ -102,8 +102,8 @@ def genetic(popMaxSize, iter, crossoverRate, mutationRate):
             bs = s
     return bs
 
-crossoverRate = 0.75
+crossoverRate = 0.8
 mutationRate = 0.2
-iter = 50
-popMaxSize = 10
+iter = 50 # number of generations
+popMaxSize = 20 # size of the population
 print(genetic(popMaxSize,iter,crossoverRate,mutationRate))
